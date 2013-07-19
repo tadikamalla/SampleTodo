4098fe9f9e5e7f1f7ae2635b5948507426e64c65
ghorsey <geoff@shhhinnovations.com>
2013-07-19 14:44:35 -0700

    Initial commit

---
+++ SampleTodo.Web/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿namespace SampleTodo.Web
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Http;
+
+    /// <summary>
+    /// Web API Configuration
+    /// </summary>
+    public static class WebApiConfig
+    {
+        /// <summary>
+        /// Registers the specified config.
+        /// </summary>
+        /// <param name="config">The config.</param>
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+        }
+    }
+}
---
+++ SampleTodo/Domain/Todo.cs
@@ -0,0 +1,15 @@
+﻿namespace SampleTodo.Domain
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Todo
+    {
+        public virtual int Id { get; set; }
+        public virtual string Task { get; set; }
+        public virtual bool IsComplete { get; set; }
+    }
+}
