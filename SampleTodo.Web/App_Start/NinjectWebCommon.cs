9dde5f569466feef95ca0846edae994461f790a5
ghorsey <geoff@shhhinnovations.com>
2013-07-22 12:06:02 -0700

    added di for web api controllers.

--- SampleTodo.Web/App_Start/NinjectWebCommon.cs
+++ SampleTodo.Web/App_Start/NinjectWebCommon.cs
@@ -6,11 +6,12 @@ namespace SampleTodo.Web.App_Start
     using System;
     using System.Diagnostics.CodeAnalysis;
     using System.Web;
+    using System.Web.Http;
     using Microsoft.Web.Infrastructure.DynamicModuleHelper;
     using Ninject;
     using Ninject.Web.Common;
-
     using SampleTodo.Repository;
+    using SampleTodo.Web.Resolvers;
 
     /// <summary>
     /// The dependency injection bootstrap component
@@ -52,6 +53,9 @@ namespace SampleTodo.Web.App_Start
             kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
             
             RegisterServices(kernel);
+
+            GlobalConfiguration.Configuration.DependencyResolver = new NinjectResolver(kernel);
+
             return kernel;
         }
 
---
+++ SampleTodo.Web/Resolvers/NinjectResolver.cs
@@ -0,0 +1,39 @@
+﻿namespace SampleTodo.Web.Resolvers
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Web.Http.Dependencies;
+    using Ninject;
+
+    /// <summary>
+    /// Ninject Resolver
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "Reviewed.")]
+    public class NinjectResolver : NinjectScope, IDependencyResolver
+    {
+        /// <summary>
+        /// The _kernel
+        /// </summary>
+        private readonly IKernel kernel;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NinjectResolver" /> class.
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        public NinjectResolver(IKernel kernel)
+            : base(kernel)
+        {
+            this.kernel = kernel;
+        }
+
+        /// <summary>
+        /// Starts a resolution scope.
+        /// </summary>
+        /// <returns>
+        /// The dependency scope.
+        /// </returns>
+        public IDependencyScope BeginScope()
+        {
+            return new NinjectScope(this.kernel.BeginBlock());
+        }
+    }
+}
