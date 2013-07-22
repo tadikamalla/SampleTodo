bd893a04e279543e458fef6ff6ddbc8f29f9a807
ghorsey <geoff@shhhinnovations.com>
2013-07-22 11:25:22 -0700

    working on setting up test app di.

---
+++ SampleTodo.Web/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿namespace SampleTodo.Web.Controllers
+{
+    using System.Web.Mvc;
+    using SampleTodo.Repository;
+
+    public class HomeController : Controller
+    {
+        private readonly ITodoRepository todoRepository;
+
+        public HomeController(ITodoRepository todoRepository)
+        {
+            this.todoRepository = todoRepository;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SampleTodo.Web/Controllers/TodosController.cs
@@ -0,0 +1,43 @@
+﻿namespace SampleTodo.Web.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Http;
+    using SampleTodo.Repository;
+
+    public class TodosController : ApiController
+    {
+        private readonly ITodoRepository todoRepository;
+
+        public TodosController(ITodoRepository todoRepository)
+        {
+            this.todoRepository = todoRepository;
+        }
+
+        // GET api/todo
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/todo/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/todo
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/todo/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/todo/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
